dc3855f5fd6a61c266250ac6f8dfb4f78090f953
silent-301 <silentrehman7866@gmail.com>
2026-07-19 04:32:42 +0500

    is not running Commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
-    message: "CI/CD Pipeline ",
+    message: "CI/CD Pipeline is not running",
   });
 });
 
